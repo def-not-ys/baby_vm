e08410a937e4f57faa38f25bc4ca14509bcc9d43
def-not-ys <luys1201@gmail.com>
2021-09-06 14:24:59 -0700

    refactored memory and hashmap

---
+++ src/memory.h
@@ -0,0 +1,65 @@
+#ifndef MEM_H
+#define MEM_H
+
+#include "baby_vm.h"
+
+#define TEXT_REGION_START   0x0003 // first 3 slots are reserved
+#define TEXT_REGION_LAST    0x0fff // @TODO: revisit this!
+#define TEXT_REGION_SIZE    ( TEXT_REGION_LAST - TEXT_REGION_START )
+#define DATA_REGION_START   0x1000
+#define DATA_REGION_LAST    0x13ff
+#define DATA_REGION_SIZE    ( DATA_REGION_LAST - DATA_REGION_START )
+
+#define STACK_REGION_START  0xfbff
+#define HEAP_REGION_START   0x1400
+
+// memory model
+/* 65535 (2^16) bytes
+    0xffff   +----------+
+             | Reserved |   1024 bytes
+             |          |
+    0xfbff   +----------+
+             |          |
+             |  Stack   |
+             |          |
+             +----+-----+
+             |    |     |
+             |    v     |
+             |          |
+             |    ^     |
+             |    |     |
+             +----+-----+
+             |   Heap   |
+             |          |
+    0x1400   +----------+
+             |   Data   |
+             |          |
+    0x1000   +----------+
+             |          |
+             |   Text   |
+             |          |
+    0x0000   +----------+
+
+    opcode is omiited as all instructions have the same opcode
+    instruction structure:
+                (OP)    addr.a  addr.b  addr.c
+    in bytes:   0       2       2       2       -> 6 byte instruction
+
+    @TODO: revisit the text section, may need more space for text region
+
+    reserve 0x0fff for HALT instruction
+    total text region in bytes: (255 (0xfff) - 3) * 2 byte/slot (uint16_t) = 504 bytes = 6 * 84
+    maximum of 84 + 1(HALT) = 85 instructions
+
+*/
+
+typedef enum
+{
+    UINITIALIZED,
+    SECTION,
+    TEXT,
+    DATA,
+    INVALID
+} Section;
+
+#endif // MEM_H
---
+++ util/hashmap.h
@@ -0,0 +1,43 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "../src/memory.h"
+
+#define IDX_BASE            STACK_REGION_START
+#define IDX_LAST            0xffff
+#define SIZE_OF_DATA        8                                           // this will only work with size_t = 4 byte
+#define HASH_TABLE_SIZE     ( ( IDX_LAST - IDX_BASE ) / SIZE_OF_DATA )  // 128 slots
+#define INVALID_INDEX       HASH_TABLE_SIZE                             // use table size as marker for invalid index
+#define SLOT_UNUSED         0x0000
+#define SLOT_DELETED        IDX_LAST                                    // use last index as marker for deleted slot
+
+#define DEBUG_MODE          1
+
+typedef enum
+{
+    STATUS_OK,
+    STATUS_ERR,
+    STATUS_FULL
+} HashmapStatus;
+
+typedef struct _hashmap
+{
+    uint16_t        (*find)(struct _hashmap* self, const char* label);
+    HashmapStatus   (*insert)(struct _hashmap* self, const char* label, uint16_t addr);
+    uint16_t        (*delete)(struct _hashmap* self, const char* label);
+    void            (*clear)(struct _hashmap* self);
+    Data*           _data;
+} Hashmap;
+
+void hashmap_init(Hashmap* self, const Memory* ptr_memory);
+
+#if DEBUG_MODE
+void hashmap_test(Hashmap* self);
+#endif // DEBUG_MODE
+
+#endif // HASHMAP_H
