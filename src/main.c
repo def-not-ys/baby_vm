c07719dfb7d3ce570f55b70d2e9001e77f844b3c
def-not-ys <luys1201@gmail.com>
2022-09-18 15:01:02 -0400

    add flag capability

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
+#include <unistd.h>
 #include "baby_vm.h"
+
+// flags 
+int file_flg = 0;
+int arg_flg = 0;
+int verbose_flg = 0;
+int help_flg = 0;
 
 int main(int argc, char* argv[])
 {
-    char* buffer = NULL;
+    int opt = 0;
+    char* file_path = NULL;
     Arguments args = {0};
 
     /*
@@ -11,36 +19,71 @@ int main(int argc, char* argv[])
      *  arguments supplied to the program to be executed (OPTIONAL)
      */
 
-    if (argc < 2)
-    {
-        LOG("Please enter file to be executed, or press q to quit");
-        wait_for_data(&buffer, &args);
-    }
-    else
-    {
-        if (strlen(argv[1]) >= MAX_BUFFER_LEN)
-        {
-            // for simplicity. do not support dynamically allocate long string
-            LOG("argument too long. exceeds maximum argument length. exiting gracefully...");
-            exit(EXIT_FAILURE);
+    int err_flg = 1;
+    while ((opt = getopt(argc, argv, ":f:a:vh")) != -1) {
+        printf("%c\n", opt);
+        switch(opt) {
+        case 'v':
+            printf("received verbose flag -v\n");
+            verbose_flg++;
+            err_flg = 0;
+            break;
+        case 'f':
+            printf("received file flag -f: %s\n", optarg);
+            if (strlen(optarg) >= MAX_BUFFER_LEN)
+            {
+                // for simplicity. do not support dynamically allocate long string
+                LOG("argument too long. exceeds maximum argument length. exiting gracefully...");
+                exit(EXIT_FAILURE);
+            }
+            args.list = &argv[2];
+            args.len = argc - 2;
+            file_path = optarg;
+            err_flg = 0;
+            break;
+        case 'a':
+            printf("received argument flag -f: %s\n", optarg);
+            // @TODO: parse arguments
+            // args.list = &argv[2];
+            // args.len = argc - 2;
+            err_flg = 0;
+            break;
+        case 'h':
+            printf("help menu\n");
+            err_flg = 0;
+            break;
+        case ':':       /* -f or -o without operand */
+            fprintf(stderr,
+                "Option -%c requires an operand\n", optopt);
+            err_flg = 1;
+            break;
+        case '?':
+            fprintf(stderr,
+                "Unrecognized option: '-%c'\n", optopt);
+            err_flg = 1;
         }
-
-        buffer = argv[1];
-
-        args.list = &argv[2];
-        args.len = argc - 2;
-
+    }
+    if (err_flg) {
+        fprintf(stderr, "usage: baby_vm -f <file-path> [-v] [-a] [-h (help)]");
+        exit(2);
+    }
+    for ( ; optind < argc; optind++) {
+        if (access(argv[optind], R_OK)) {
+            printf("%s\n", argv[optind]);
+        } else {
+            printf("R not ok\n");
+        }
     }
 
-#if DEBUG_ON
-    LOG("after assigning new value to buffer");
-    LOG(buffer);
-    LOG("arg.list: ");
-    LOG(*(args.list));
-    LOG_COMP(MAX(argc-2, 0), args.len);
-#endif // DEBUG_ON
+    if (verbose_flg) {
+        LOG("after assigning new value to buffer");
+        LOG(file_path);
+        LOG("arg.list: ");
+        LOG(*(args.list));
+        LOG_COMP(MAX(argc-2, 0), args.len);
+    }
 
-    int16_t rv = process(&args, &buffer);
+    int16_t rv = process(&args, &file_path);
 
     // @TODO: need a better way to handle return value.
     // display return value
